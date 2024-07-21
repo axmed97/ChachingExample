f0dcdd0d8e1557a97dd388925cb120d6412c2861
axmed97 <axmed.dev.13@gmail.com>
2024-07-21 16:52:35 +0400

    Add project files.

---
+++ InMemory/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        //[HttpGet("[action]")]
+        //public void Set()
+        //{
+        //    _memoryCache.Set("name", "axmed");
+        //}
+
+        //[HttpGet("[action]")]
+        //public string Get()
+        //{
+        //    //return _memoryCache.Get<string>("name");
+        //    if (_memoryCache.TryGetValue<string>("name", out string value))
+        //    {
+        //        return value;
+        //    }
+        //    return string.Empty;
+        //}
+
+
+        [HttpGet("[action]")]
+        public void Set()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30), // Total
+                SlidingExpiration = TimeSpan.FromSeconds(5) // Period
+            });
+        }
+
+        [HttpGet("[action]")]
+        public DateTime Get()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+    }
+}
---
+++ Redis/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Redis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public ValuesController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Set(string name, string surname)
+        {
+            await _distributedCache.SetStringAsync("name", name, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            await _distributedCache.SetAsync("surname", Encoding.UTF8.GetBytes(surname));
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Get()
+        {
+            var name = await _distributedCache.GetStringAsync("name");
+            var nameBinary = await _distributedCache.GetAsync("surname");
+            var surname = Encoding.UTF8.GetString(nameBinary);
+            return Ok(new
+            {
+                name,
+                surname,
+                nameBinary
+            });
+        }
+
+    }
+}
